64544f0dc87eaba4ccadd4c1cec1036e467bf439
aayushchhetri2 <aayush.b.lamichhane@gmail.com>
2024-01-27 12:00:35 +0545

    navbar added

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import  './NavBar.css'
+import {Link} from 'react-scroll'
+import contactImg from '../../assests/contactImg.png'
+
+const NavBar = () => {
+  return (
+    <nav className='navbar'>
+      <h1 className='logo'>Aayush</h1>
+    <div className='desktopMenu'>
+      <Link className='desktopMenuListItem'>Home</Link>
+      <Link className='desktopMenuListItem'>About</Link>
+      <Link className='desktopMenuListItem'>Project</Link>
+    </div>
+    <button className='desktopMenuBtn'>
+      <img src={contactImg} alt=''  className='desktopMenuImg'/>Contact Me</button>
+    </nav>
+  )
+}
+
+export default NavBar
