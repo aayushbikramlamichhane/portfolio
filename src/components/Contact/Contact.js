49c6d37343c43ef2bd5ae8af0ede73a3cef667eb
aayushchhetri2 <aayush.b.lamichhane@gmail.com>
2024-01-28 16:27:52 +0545

    email linked to contact me form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import './Contact.css'
 import gi from '../../assests/git.png'
 import li from '../../assests/lin.png'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_4uhjr9g', 'template_jwwc6u6', form.current, 'xrhbrGMskQRdhnm3p')
+            .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+      };
 return (
         <div id='contactPage'>
         <div id='contact'>
             <h1 className='contactPageTitle'>Contact Me</h1>
             <span className='contactDesc'>Please fill out the form below to discuss any work opportunities.</span>
-            <form className='contactForm'>
-                <input type='text' className='name' placeholder='Your Name'/>
-                <input type='email' className='email' placeholder='Your Email'/>
+            <form className='contactForm' ref={form} onSubmit={sendEmail}>
+
+                <input type='text' className='name' placeholder='Your Name' name='user_name'/>
+                <input type='email' className='email' placeholder='Your Email' name='user_email'/>
                 <textarea className='msg' name='message' rows="5" placeholder='Your Message'></textarea>
-                <button type='submit' value='Send' className='submitBtn'>Submit</button>
+                <button type='submit' value='Send' className='submitBtn' >Submit</button>
                 <div className='links'>
-                    <img src={gi} alt='' className='link'/>
-                    <img src={li}  alt='' className='link'/>
+                    <a href='https://github.com/aayushbikramlamichhane' target='__blank'>
+                    <img src={gi} alt='' className='link'/></a>
+                    <a href='https://www.linkedin.com/in/aayush-l-c-88ba79179/' target='__blank'>
+                    <img src={li}  alt='' className='link'/></a>
                 </div> 
             </form>
         </div>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  './NavBar.css'
 import {Link} from 'react-scroll'
 import contactImg from '../../assests/contactImg.png'
+import menu from '../../assests/ham.png'
 
 const NavBar = () => {
+  const [showMenu,setShowMenu] = useState(false);
   return (
+    
     <nav className='navbar'>
       <h1 className='logo'>Aayush</h1>
     <div className='desktopMenu'>
@@ -13,15 +16,16 @@ const NavBar = () => {
       <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Skills</Link>
       <Link activeClass='active' to='portfolio-container' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Portfolio</Link>
     </div>
-    <button className='desktopMenuBtn' onClick={()=> 
-        document.getElementById('contact').scrollIntoView({behavior:'smooth' ,offset:-100}) }>
+    <button className='desktopMenuBtn'  onClick={()=> 
+        document.getElementById('contact').scrollIntoView({behavior:'smooth' })  } offset={100} >
       <img src={contactImg} alt=''  className='desktopMenuImg'/>Contact Me</button>
-    <div className='navMenu'>
-      <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='ListItem'>Home</Link>
-      <Link activeClass='active' to='abt-container' spy={true} smooth={true} offset={-100} duration={500} className='ListItem'>About</Link>
-      <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className='ListItem'>Skills</Link>
-      <Link activeClass='active' to='portfolio-container' spy={true} smooth={true} offset={-100} duration={500} className='ListItem'>Portfolio</Link>
-      <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-100} duration={500} className='ListItem'>Contact</Link>
+      <img src={menu} alt='' className='mobMenu' onClick={()=>setShowMenu(!showMenu)}/>
+    <div className='navMenu' style={{display: showMenu? 'flex':'none'}}>
+      <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowMenu(false)}>Home</Link>
+      <Link activeClass='active' to='abt-container' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowMenu(false)}>About</Link>
+      <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowMenu(false)}>Skills</Link>
+      <Link activeClass='active' to='portfolio-container' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowMenu(false)}>Portfolio</Link>
+      <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowMenu(false)}>Contact</Link>
     </div>
     </nav>
   )
